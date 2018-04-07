f4f6e5c3bfe15c0b80f5c9bd9802de2b9d71935b
kryptokinght <minanshu.1998@gmail.com>
2018-04-08 00:00:01 +0530

    add example for singleton

---
+++ singleton.js
@@ -0,0 +1,30 @@
+/*
+Ensure there is only one instance of that class for example an object that 
+manages databse management pool.
+*/
+
+var Singleton = (function () {
+    var instance;
+ 
+    function createInstance() {
+        var object = new Object("I am the instance");
+        return object;
+    }
+ 
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+ 
+function run() {
+ 
+    var instance1 = Singleton.getInstance();
+    var instance2 = Singleton.getInstance();
+ 
+    alert("Same instance? " + (instance1 === instance2));  
+}
