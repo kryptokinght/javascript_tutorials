f8e1f3fc883f655915f28bfc6f26389391bbe918
Minanshu Singh <minanshu.1998@gmail.com>
2018-12-14 00:51:53 +0530

    remove unnecessary files

--- commonjs_module_example/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-const module1 = require('./module1');
-
-module1.func();
--- commonjs_module_example/module1.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	func: function() {
-		let a = 10, b = 20;
-		console.log("module1 function loaded!!");
-		console.log(a+b);
-	} 
-};
